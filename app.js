496fee317475a662003bad030a66c2978cbbbbdd
Yin <yinstanleycheung@gmail.com>
2022-07-12 18:49:02 -0700

    Connect /articles route to wikiDB

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('view engine', "ejs");
 app.use(express.static("public"));
 
 main().catch(err => console.log(err));
+
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/wikiDB');
 }
@@ -18,9 +19,18 @@ const articleSchema = new mongoose.Schema({
   title: String,
   content: String
 })
+
 const Article = mongoose.model("Article", articleSchema);
 
-
+app.get("/articles", (req, res) => {
+  Article.find({}, (err, foundArticles) => {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  })
+});
 
 app.listen(port, () => {
   console.log("App running on port " + port);
