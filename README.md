c0dd29ef8fdfe353db1db47dab32e10e6a603c08
Yin <yinstanleycheung@gmail.com>
2022-07-12 18:25:51 -0700

    Add starting files

---
+++ README.md
@@ -0,0 +1,5 @@
+# WikiDB
+
+Practice project for a [Udemy course](https://www.udemy.com/course/the-complete-web-development-bootcamp/).
+
+
---
+++ app.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', "ejs");
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/wikiDB');
+}
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+const Article = mongoose.model("Article", articleSchema);
+
+
+
+app.listen(port, () => {
+  console.log("App running on port " + port);
+});
